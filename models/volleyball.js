a99464c6f98b9156211132572e026c31dddb2feb
JenyZ <jenniferzirk@gmail.com>
2019-05-05 13:49:09 -0400

    initial commit

--- models/volleyball.js
+++ models/volleyball.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const volleyballSchema = new Schema({
+const scheduleSchema = new Schema({
   opponent: { type: String, required: true },
   location: { type: String, required: true },
   startTime: Date,
   date: { type: Date, default: Date.now }
 });
 
-const Volleyball = mongoose.model("Volleyball", volleyballSchema);
+const Volleyball = mongoose.model("Volleyball", scheduleSchema);
 
 module.exports = Volleyball;
